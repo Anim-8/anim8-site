67c8269825b43f99145c57b94dd01f079eb48eb4
spsa8 <samuel.steinberg@anim8.tech>
2024-06-16 18:32:15 -0500

    rm apostrophes

--- src/app/(home)/MissionStatement.tsx
+++ src/app/(home)/MissionStatement.tsx
@@ -8,10 +8,10 @@ const MissionStatement = () => {
             <SubHeader className='mt-20 mb-5'>Quality and Process Control</SubHeader>
             <p>
                 Shift the paradigm in your Production Standards with Our Solutions.
-                In today's competitive market, achieving exceptional quality and precise process control
+                In today&apos;s competitive market, achieving exceptional quality and precise process control
                 is the single most important factor in successful manufacturing. At Anim8, we understand that a
                 deterministic process and narrow standard deviations are key to consistency and excellence.
-                That's why we specialize in delivering top-tier quality and process control services designed to
+                That&apos;s why we specialize in delivering top-tier quality and process control services designed to
                 propel your business to new heights.
             </p>
         </Statement>
