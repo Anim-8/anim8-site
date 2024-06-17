b63b91e4fc862548ea7e19983fd79489fe308463
spsa8 <samuel.steinberg@anim8.tech>
2024-06-17 09:37:05 -0500

    fix background

--- src/app/components/nav/Navigation.tsx
+++ src/app/components/nav/Navigation.tsx
@@ -21,7 +21,7 @@ const Navigation: React.FC = () => {
                     </button>
                 </div>
                 <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${!show && "hidden"}`} id="navbar-sticky">
-                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700" style={{background: 'none'}}>
                         <li>
                             <a href="#home" className="block py-2 px-3 text-white rounded md:bg-transparent md:p-0">
                             Home
