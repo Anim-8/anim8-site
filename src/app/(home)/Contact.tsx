8aa56436bfb228c5d51cc37538e5956a64657cc5
Roberto Calvi <roberto.calvi@anim8.tech>
2024-06-18 20:59:35 -0500

    updated contact infos

--- src/app/(home)/Contact.tsx
+++ src/app/(home)/Contact.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import SubHeader from '../components/shared/SubHeader'
-import styles from './page.module.scss'
+import Image from 'next/image'
 
 const Contact = () => {
     return (
         <div className="w-full flex flex-col items-center justify-center mt-20" id="contact">
             <SubHeader className='mb-4'>Contact</SubHeader>
-            <div>
-                <ul>
-                    <li>samuel.steinberg@anim8.tech</li>
-                    <li>roberto.calvi@anim8.tech</li>
-                </ul>
+            <div className="flex items-center">
+                <div className='mt-2.5'>
+                    <Image  src="/mail.svg" alt="email icon" width={50} height={25} />
+                </div>
+                <span>info@anim8.tech</span>
             </div>
             <div className='mt-10 text-center'>
                 More contact information will be added shortly.
