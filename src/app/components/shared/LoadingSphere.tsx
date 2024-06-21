01b7c06b7711ae24c0b1b16efa98f12d74f47705
spsa8 <samuel.steinberg@anim8.tech>
2024-06-21 14:07:10 -0500

    sphere

--- src/app/components/shared/LoadingSphere.tsx
+++ src/app/components/shared/LoadingSphere.tsx
@@ -1,19 +1,13 @@
 "use client"
 import React, { useRef } from 'react'
-import { Environment } from '@react-three/drei'
 import { MeshStandardMaterial, SphereGeometry, Mesh } from 'three'
 
 const sphere = new SphereGeometry(.2, 64, 64)
-const material = new MeshStandardMaterial({color: 0x00ffef, roughness: .1, metalness: .2})
+const material = new MeshStandardMaterial({ color: 0x00ffef, roughness: .1, metalness: .2 })
 
 const LoadingSphere = () => {
-    const ref = useRef<Mesh>(null)
-  return (
-    <>
-    <mesh geometry={sphere} material={material} ref={ref}/>
-    <Environment preset="apartment" blur={0.9} />
-    </>
-  )
+  const ref = useRef<Mesh>(null)
+  return <mesh geometry={sphere} material={material} ref={ref} />
 }
 
 export default LoadingSphere
