2b33005045d558b2a1508126e67c393e619e24c3
spsa8 <samuel.steinberg@anim8.tech>
2024-06-22 21:51:20 -0500

    inpur range

---
+++ src/app/components/shared/InputRange.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type InputProps = React.HTMLProps<HTMLInputElement>
+
+const InputRange = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+    return <input className='range' type="range" ref={ref} { ...props } />
+})
+
+export default InputRange
