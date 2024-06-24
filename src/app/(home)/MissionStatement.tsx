3a49a5ebb500ef675ad8d64032399d175c9fb038
spsa8 <samuel.steinberg@anim8.tech>
2024-06-23 21:05:32 -0500

    special hover color

--- src/app/(home)/MissionStatement.tsx
+++ src/app/(home)/MissionStatement.tsx
@@ -5,8 +5,8 @@ import InputRange from '../components/shared/InputRange'
 import { useRef, useState } from 'react'
 import CardData from '@/models/CardData'
 import Cards from './Cards'
-import useDimensions from '@/hooks/useDimensions'
 import styles from "./page.module.scss"
+import useDimensions from '@/hooks/useDimensions'
 
 const cards: CardData[] = [
     { title: "Manual", content: <p className='secondary'>A manual business is typically characterized as having many laboring and repetitive tasks. The decision making process is mostly reactionary, with no significant usage of the past to hep better comprehend the present.</p> },
@@ -14,7 +14,7 @@ const cards: CardData[] = [
     { title: "Animated", content: <p className='secondary'>A business reaches the animated state when it achieves a high degree of automation, but more importantly portion a large part of the decision making process is handled by software and predictive analysis.</p> }
 ]
 
-const MissionStatement = () => {
+const MissionStatement: React.FC = () => {
     const ref = useRef<any>()
     const { width } = useDimensions(ref)
     const [value, setValue] = useState<number>(50)
@@ -22,17 +22,20 @@ const MissionStatement = () => {
         <div className="w-full text-center" ref={ref}>
             <Statement id="quality">
                 <div className="relative inline-block text-center">
-                    <div className={styles.whiteCircle} ></div>
-                    <SubHeader className='secondary mt-20 mb-5 z-10'> Animate </SubHeader>
+                    <SubHeader className='secondary mt-20 mb-5 z-10'>Animate</SubHeader>
                 </div>
-                <div className='secondary p-4' > Our goal is to help your company go up the animation spectrum. </div>
-                <div>
-                    <div className="max-w-screen-l flex flex-wrap items-center justify-between mx-8 p-3">
-                        <div className='text-2xl'> Manual </div>
-                        <div className='text-2xl'> Automated </div>
-                        <div className='text-2xl'> Animated </div>
-                    </div>
-                    <div className="mb-4 px-3">
+                <div className='secondary p-4'>Our goal is to help your company go up the animation spectrum.</div>
+                <div className='max-w-screen-l flex flex-col mx-8 p-3'>
+                    {
+                        width >= 600 && (
+                            <div className="flex justify-between">
+                                <div className={`text-2xl ${value <= 25 ? "special" : ""}`}>Manual</div>
+                                <div className={`text-2xl ${value >= 26 && value <= 75 ? "special" : ""}`}>Automated</div>
+                                <div className={`text-2xl ${value > 75 ? "special" : ""}`}>Animated</div>
+                            </div>
+                        )
+                    }
+                    <div className="mb-4">
                         <InputRange value={value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(Number(e.target.value))} min="0" max="100" />
                     </div>
                 </div>
@@ -42,7 +45,7 @@ const MissionStatement = () => {
                     describe the transition from automated work to animated. Here are the three states:
                 </p>
             </Statement>
-            <Cards cards={cards} animated={value <= 25 ? cards[0].title : value <= 75 ? cards[1].title : cards[2].title}/>
+            <Cards cards={cards} animated={value <= 25 ? cards[0].title : value <= 75 ? cards[1].title : cards[2].title} />
         </div>
     )
 }
