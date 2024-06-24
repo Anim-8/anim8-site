774915326be649c815dd8f705e19ecc8cdf55494
spsa8 <samuel.steinberg@anim8.tech>
2024-06-23 21:20:57 -0500

    use motion scale instead

--- src/app/(home)/Cards.tsx
+++ src/app/(home)/Cards.tsx
@@ -3,6 +3,7 @@ import React, { memo } from 'react'
 import HexCard from '../components/shared/HexCard';
 import styles from './page.module.scss'
 import { alphanumeric } from '@/custom-types';
+import { motion } from 'framer-motion';
 
 type CardsProps = {
     cards: CardData[];
@@ -16,15 +17,14 @@ const Cards:React.FC<CardsProps> = memo(({ cards, width, height, animated = "", 
     return (
         <div className={`flex w-full flex-col lg:flex-row ${className}`}>
             {cards.map(card => (
-                <div key={card.title} className={`lg:basis-1/3 ${styles.essentialCard} flex justify-center`}>
+                <motion.div key={card.title} className={`lg:basis-1/3 ${styles.essentialCard} flex justify-center`} animate={{scale: animated === card.title ? 1.1 : 1}}>
                     <HexCard 
                         title={card.title} 
                         content={card.content} 
-                        isAnimated={card.title === animated}
                         width={width}
                         height={height}
                     />
-                </div>
+                </motion.div>
             ))}
         </div>
     )
