ad5f3e9f154b46314e0475bbdc4813bd6c34209c
Roberto Calvi <roberto.calvi@anim8.tech>
2024-06-24 22:53:10 -0500

    business section to grid

--- src/app/(home)/Business.tsx
+++ src/app/(home)/Business.tsx
@@ -1,16 +1,12 @@
 "use client"
 import Image from 'next/image'
-import { motion } from 'framer-motion'
-import HexCard from '../components/shared/HexCard'
 import Statement from '../components/shared/Statement'
 import SubHeader from '../components/shared/SubHeader'
 import CardData from '@/models/CardData'
 import BusinessRow from './BusinessRow'
 import useDimensions from '../../hooks/useDImensions'
-import { useMemo, useRef } from 'react'
-import Cards from './Cards'
+import { useRef } from 'react'
 import styles from "./page.module.scss"
-import HexCardBlurred from '../components/shared/HexCardBlurred'
 
 const cards: CardData[] = [
     {
@@ -46,11 +42,10 @@ const hoverConfig = { y: -50, scale: 1.1 }
 const Business = () => {
     const ref = useRef<any>()
     const { width } = useDimensions(ref)
-    const aspect: string | undefined = useMemo(() => width < 430 ? "85%" : undefined, [width])
     return (
         <div className='mt-10 lg:mt-0 w-full' id="business">
             <Statement>
-                <SubHeader className='mb-4'>Business</SubHeader>
+                <SubHeader className='mb-4 py-8'>Business</SubHeader>
                 <p className='text-sm'>
                     We believe that simple ideas are the most powerful, so we personify businesses into a model that we call the brain and body. The brain and nervous system represent the software,
                     storage and data pipelines of the business and the body represents the people, equipment and real-estate.
@@ -59,34 +54,19 @@ const Business = () => {
                 </p>
 
             </Statement>
-            <div className='flex flex-col w-full' ref={ref}>
-                <BusinessRow url="/brainBody.webp" headerOne='Body' descriptionOne={descriptions.descriptionOne} headerTwo='Brain' descriptionTwo={descriptions.descriptionTwo} />
-                {
-                    width >= 768 ? (
-                        <div className='flex flex-row text-center items-center'>
-                            <motion.div
-                                className='lg:basis-1/3 flex justify-center'
-                                whileHover={hoverConfig}
-                            >
-                                <HexCardBlurred title={cards[0].title} content={cards[0].content} width={aspect} height={aspect} />
-                            </motion.div>
-                            <div className="lg:basis-1/3 flex justify-center">
-                                <Image src="/body.webp" alt="body.webp" width={165} height={372} />
-                            </div>
-                            <motion.div
-                                className='lg:basis-1/3 flex justify-center'
-                                whileHover={hoverConfig}
-                            >
-                                <HexCardBlurred title={cards[1].title} content={cards[1].content} width={aspect} height={aspect} />
-                            </motion.div>
-                        </div>
-                    ) : <Cards cards={cards} width={aspect} height={aspect} />
-               }
-               <div className={styles.greyCircle}>
-                <div className={styles.orbShadow}></div>
-                <div className={styles.bodyShadow}></div>
-                <div className={styles.orbShadow}></div>
-               </div>
+            <div className={styles.gridContainer} ref={ref}>
+                <BusinessRow header='Body' description={descriptions.descriptionOne} card={cards[0]} width={width} hoverConfig={hoverConfig}/>
+                <div className='flex justify-center'>
+                    <Image src="/brainBody.webp" alt="body.webp" width={165} height={372} />
+                </div>
+                <BusinessRow header='Brain' description={descriptions.descriptionTwo} card={cards[1]} width={width} hoverConfig={hoverConfig}/>
+            </div>
+            <div className='py-12'>
+                <div className={styles.greyCircle}>
+                        <div className={styles.orbShadow}></div>
+                        <div className={styles.bodyShadow}></div>
+                        <div className={styles.orbShadow}></div>
+                    </div>
             </div>
         </div>
     )
