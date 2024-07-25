560feb94b3eef0cf9fe8c32142d47417216d42a8
spsa8 <samuel.steinberg@anim8.tech>
2024-07-25 12:36:18 -0500

    styles

--- src/app/(home)/Landing.tsx
+++ src/app/(home)/Landing.tsx
@@ -1,13 +1,14 @@
+"use client"
 import Image from "next/image"
 import styles from "./page.module.scss"
+import HomeCanvas from "../components/home/HomeCanvas"
 
 const Landing = () => {
   return (
     <div className={styles.heroContainer} id="home">
-        <div className={styles.leftRectangle}></div>
-        <div className={styles.rightRectangle}></div>
-        <div className="flex justify-center items-center h-screen">
-          <Image src="/hero_logo.webp" alt="hero logo" width={700} height={700} priority quality={100} style={{ maxWidth: '70%', marginTop: '8%'}}/>
+      <HomeCanvas />
+        <div className="flex justify-center items-center h-screen absolute">
+          <Image src="/hero_logo.webp" alt="hero logo" width={700} height={700} priority quality={100} style={{ maxWidth: '70%', marginTop: '8%'}} />
         </div>
     </div>
   )
